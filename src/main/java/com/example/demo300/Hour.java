5d30b8e5a1f2954c021daa4b7b8c5d1f941e8db0
victorvic77 <60477725+victorvic77@users.noreply.github.com>
2021-05-18 20:19:04 +0300

    Добавил класс Hour

--- src/main/java/com/example/demo300/Hour.java
+++ src/main/java/com/example/demo300/Hour.java
@@ -9,14 +9,13 @@ import lombok.*;
 
 @Data
 @Entity
-public class Day {
+public class Hour {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Double Day;
+    private Double Time;
 
     private Double Open;
     private Double High;
     private Double Low;
     private Double Close;
-
 }
